849defa34ac5694219980c4f63d6e8ed93212fcc
FurryAxolotl <furryaxolotl@courvix.com>
2023-07-12 15:48:20 +0000

    vite, react and typescript template

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import * as BABYLON from "@babylonjs/core";
+import "@babylonjs/loaders/glTF";
+import "pepjs";
+import SceneComponent from "babylonjs-hook";
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+	let box;
+	
+	function onSceneReady(scene: BABYLON.Scene) {
+		const canvas = scene.getEngine().getRenderingCanvas();
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+		const camera = new BABYLON.ArcRotateCamera("camera", -Math.PI / 2, Math.PI / 2.5, 3, BABYLON.Vector3.Zero(), scene);
+		camera.attachControl(canvas, true);
+
+		const light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(0, 1, 0), scene);
+
+		box = BABYLON.MeshBuilder.CreateBox("box", {}, scene);
+	}
+
+	function onRender(scene: BABYLON.Scene) {
+		const deltaTimeInMillis = scene.getEngine().getDeltaTime();
+
+		const rpm = 10;
+
+		box.rotation.y += (rpm / 60) * Math.PI * 2 * (deltaTimeInMillis / 1000);
+	}
+	
+	return <SceneComponent className="w-screen h-auto focus:outline-none touch-none" antialias onSceneReady={onSceneReady} onRender={onRender} />;
 }
-
-export default App
